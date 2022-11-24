2230442a6cb2267dba3c9c7a6d56dd1697782828
Lesly Phiri <97601156+phirilesly@users.noreply.github.com>
2022-11-24 18:42:57 +0200

    Update RecipeStep.cs

--- RecipesWebApi/RecipesGuides.Abstraction/RecipeStep.cs
+++ RecipesWebApi/RecipesGuides.Abstraction/RecipeStep.cs
@@ -1,1 +1,15 @@
+using System;
 
+namespace RecipesGuides.Abstraction
+{
+    public class RecipeStep
+    {
+          public string StepNumber
+    { get; set; }
+        
+          public string StepDescription
+    { get; set; }
+        
+   
+    }
+}

7d125b2ce9c56d07c698d98349526f5191f7d247
Lesly Phiri <97601156+phirilesly@users.noreply.github.com>
2022-11-24 15:12:04 +0200

    added recipe class

---
+++ RecipesWebApi/RecipesGuides.Abstraction/Recipe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RecipesGuides.Abstraction
+{
+    public class Recipe
+    {
+          public string Name
+    { get; set; }
+        
+          public string Description
+    { get; set; }
+    }
+}
